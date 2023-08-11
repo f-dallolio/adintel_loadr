a8171f2291408668b75f832333c1c2dace477c5e
f-dallolio <86092653+f-dallolio@users.noreply.github.com>
2023-08-11 12:50:00 -0400

    Add files via upload

---
+++ data-raw/loader_tbl.R
@@ -0,0 +1,17 @@
+## code to prepare `loader_tbl` dataset goes here
+
+loader_tbl <- purrr::list_rbind(
+  purrr::map(
+    .x = loader_list,
+    .f = ~ tibble::tibble(
+      .x$filetype,
+      .x$file,
+      .x$table,
+      list(.x$col_names),
+      list(.x$col_types)
+    )
+  )
+)
+names(loader_tbl) <- names(loader_list[[1]])
+
+usethis::use_data(loader_tbl, overwrite = TRUE)
