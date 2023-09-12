3d7ebc210e3e4a33cd7bf55ac0010912f69d79dd
Filippo DallOlio <fdallolio@brocku.ca>
2023-09-12 01:45:25 -0400

    str_separate_upper

---
+++ R/str_separate_upper.R
@@ -0,0 +1,11 @@
+str_separate_upper <- function(string, sep = '_', to_lower = TRUE){
+  x_split <- stringr::str_split(string , "") %>% unlist()
+  x_upper <- x_split %>% stringr::str_detect("^[:upper:]+$")
+  x_pos <- which((x_upper - lag(x_upper)) == 1)
+  x_split[x_pos] <- paste0(sep, x_split[x_pos])
+  x_out <- paste0(x_split, collapse = "")
+  if(to_lower){
+    return(stringr::str_to_lower(x_out))
+  }
+  x_out
+}
